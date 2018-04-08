57dedae7c36b2b2bac82edd128dad90f7f2e241d
jkasuya <35503264+jkasuya@users.noreply.github.com>
2018-04-08 13:01:01 -1000

    Port Landing Page

--- app/imports/startup/server/machines.js
+++ app/imports/startup/server/machines.js
@@ -8,7 +8,7 @@ function addData(data) {
 }
 
 /** Initialize the collection if empty.
- * Created here because we cannot use new Date() in JSON */
+* Created here because we cannot use new Date() in JSON */
 if (Machines.find().count() === 0) {
   console.log('Creating default machines');
   addData({ name: 'Machine 1', dorm: 'Dorm 1', inUse: false, lastUpdated: new Date() });
@@ -17,7 +17,7 @@ if (Machines.find().count() === 0) {
   addData({ name: 'Machine 4', dorm: 'Dorm 4', inUse: false, lastUpdated: new Date() });
 }
 
-/** This subscription publishes only the documents associated with the logged in user */
+ /** This subscription publishes only the documents associated with the logged in user */
 Meteor.publish('Machine', function publish() {
   if (this.userId) {
     return Machines.find();
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className='washerwatcher-landing-background'>
+          <Grid container stackable centered columns={3}>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon size='huge' name='file text' inverted/>
+              <Header as='h1' inverted>Know When a Washing Machine is Available in Your Dorm</Header>
+              <Header as='h3' inverted>For each washer, you can see its current status</Header>
+            </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon size='huge' name='group' inverted/>
+              <Header as='h1' inverted>Individual Accounts</Header>
+              <Header as='h3' inverted>When a user creates an account, it will be personalized towards them</Header>
+            </Grid.Column>
 
-        </Grid>
+            <Grid.Column textAlign='center'>
+              <Icon size='huge' name='checked calendar' inverted/>
+              <Header as='h1' inverted>Timestamped Notes</Header>
+              <Header as='h3' inverted>The time when a washer's status changes is shown. Also, other timestamped notes,
+              such as tips or problems will also be timestamped.</Header>
+            </Grid.Column>
+
+          </Grid>
+        </div>
     );
   }
 }
