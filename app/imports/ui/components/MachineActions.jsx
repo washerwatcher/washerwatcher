18500d05c5a70d4988f02e8f4135d6788a36ffdb
jkulka <jkulka@hawaii.edu>
2018-04-19 16:35:37 -1000

    cleaned up code and machine cards

---
+++ app/imports/ui/components/MachineActions.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Button, Modal, Grid, Header, Segment, Container } from 'semantic-ui-react';
+import { Machines, MachineSchema } from '/imports/api/machine/machine';
+import { Bert } from 'meteor/themeteorchef:bert';
+import AutoForm from 'uniforms-semantic/AutoForm';
+import TextField from 'uniforms-semantic/TextField';
+import SelectField from 'uniforms-semantic/SelectField';
+import SubmitField from 'uniforms-semantic/SubmitField';
+import HiddenField from 'uniforms-semantic/HiddenField';
+import ErrorsField from 'uniforms-semantic/ErrorsField';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import NoteFeed from '../components/NoteFeed';
+import AddNote from '../components/AddNote';
+
+const inlineStyle = {
+  modal: {
+    marginTop: '0px !important',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+};
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class MachineActions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { modalOpen: false };
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleOpen() {
+    this.setState({ modalOpen: true });
+  }
+
+  handleClose() {
+    this.setState({ modalOpen: false });
+  }
+
+  /** On successful submit, insert the data. */
+  submit(data) {
+    const { name, dorm, inUse, update, lastUpdated, _id } = data;
+    Machines.update(_id, { $set: { name, dorm, inUse, update, lastUpdated } }, (error) => (error ?
+        Bert.alert({ type: 'danger', message: `Update failed: ${error.message}` }) :
+        Bert.alert({ type: 'success', message: 'Update succeeded' })));
+  }
+
+  render() {
+    return (
+        <Modal trigger={<Button onClick={this.handleOpen}>Show Modal</Button>} open={this.state.modalOpen}
+               onClose={this.handleClose} style={inlineStyle.modal}>
+          <Modal.Header>
+            Update Washer
+            <br/>
+            {this.props.machine.name} at {this.props.machine.dorm}
+          </Modal.Header>
+          <Modal.Content scrolling>
+            <Grid container centered>
+              <Grid.Column>
+                <Container>
+                  <Grid.Row>
+                    <Header as='h4' textAlign='center'>Update Status</Header>
+                    <AutoForm schema={MachineSchema} onSubmit={this.submit} model={this.props.machine}>
+                      <Segment>
+                        <SelectField name='inUse'/>
+                        <TextField name='update'/>
+                        <SubmitField value='Submit'/>
+                        <ErrorsField/>
+                        <HiddenField name='name'/>
+                        <HiddenField name='dorm'/>
+                        <HiddenField name='lastUpdated' value={new Date()}/>
+                      </Segment>
+                    </AutoForm>
+                  </Grid.Row>
+                </Container>
+                <Container>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <Header as='h4' textAlign='center'
+                              style={{ paddingTop: '12px' }}>Notes</Header>
+                      <NoteFeed
+                          notes={this.props.notes.filter(machine => machine.machineId === this.props.machine._id)}/>
+                      <Header as='h4' textAlign='center'>Add Note</Header>
+                      <AddNote machineId={this.props.machine._id}/>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Container>
+              </Grid.Column>
+            </Grid>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button negative icon='close' labelPosition='right' content='Close' onClick={this.handleClose}/>
+          </Modal.Actions>
+        </Modal>
+    );
+  }
+}
+
+/** Require a document to be passed to this component. */
+MachineActions.propTypes = {
+  machine: PropTypes.object.isRequired,
+  notes: PropTypes.array.isRequired,
+};
+
+/** Wrap this component in withRouter since we use the <Link> React Router element. */
+export default withRouter(MachineActions);
--- app/imports/ui/components/MachineCard.jsx
+++ app/imports/ui/components/MachineCard.jsx
@@ -1,34 +1,19 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Button, Card, Modal, Grid, Header, Segment, Container } from 'semantic-ui-react';
-import { Machines, MachineSchema } from '/imports/api/machine/machine';
+import { Card } from 'semantic-ui-react';
+import { Machines } from '/imports/api/machine/machine';
 import { Bert } from 'meteor/themeteorchef:bert';
-import AutoForm from 'uniforms-semantic/AutoForm';
-import TextField from 'uniforms-semantic/TextField';
-import SelectField from 'uniforms-semantic/SelectField';
-import SubmitField from 'uniforms-semantic/SubmitField';
-import HiddenField from 'uniforms-semantic/HiddenField';
-import ErrorsField from 'uniforms-semantic/ErrorsField';
 import PropTypes from 'prop-types';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Notes } from '../../api/note/note';
-import AddNote from '../components/AddNote';
-import NoteFeed from '../components/NoteFeed';
 import WasherStatus from '../components/WasherStatus';
+import MachineActions from '../components/MachineActions';
 
 function formatDate(data) {
   const options = { weekday: 'long', month: 'short', day: 'numeric' };
   return data.toLocaleTimeString('en-us', options);
 }
-
-const inlineStyle = {
-  modal: {
-    marginTop: '0px !important',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-  },
-};
 
 class MachineCard extends React.Component {
   constructor(props) {
@@ -74,56 +59,8 @@ class MachineCard extends React.Component {
               <span>{this.props.machine.update}</span>
             </Card.Meta>
           </Card.Content>
-          <Card.Content extra>
-            <Button as={Link} to={`/notes/${this.props.machine._id}`}>Notes</Button>
-            <Button floated='right' as={Link} to={`/update/${this.props.machine._id}`}>Update</Button>
-          </Card.Content>
-          <Card.Content extra>
-            <Modal trigger={<Button onClick={this.handleOpen}>Show Modal</Button>} open={this.state.modalOpen}
-                   onClose={this.handleClose} style={inlineStyle.modal}>
-              <Modal.Header>
-                Update Washer
-                <br/>
-                {this.props.machine.name} at {this.props.machine.dorm}
-              </Modal.Header>
-              <Modal.Content scrolling>
-                <Grid container centered>
-                  <Grid.Column>
-                    <Container>
-                      <Grid.Row>
-                        <Header as='h4' textAlign='center'>Update Status</Header>
-                        <AutoForm schema={MachineSchema} onSubmit={this.submit} model={this.props.machine}>
-                          <Segment>
-                            <SelectField name='inUse'/>
-                            <TextField name='update'/>
-                            <SubmitField value='Submit'/>
-                            <ErrorsField/>
-                            <HiddenField name='name'/>
-                            <HiddenField name='dorm'/>
-                            <HiddenField name='lastUpdated' value={new Date()}/>
-                          </Segment>
-                        </AutoForm>
-                      </Grid.Row>
-                    </Container>
-                    <Container>
-                      <Grid.Row>
-                        <Grid.Column>
-                          <Header as='h4' textAlign='center'
-                                  style={{ paddingTop: '12px' }}>Notes</Header>
-                          <NoteFeed
-                              notes={this.props.notes.filter(machine => machine.machineId === this.props.machine._id)}/>
-                          <Header as='h4' textAlign='center'>Add Note</Header>
-                          <AddNote machineId={this.props.machine._id}/>
-                        </Grid.Column>
-                      </Grid.Row>
-                    </Container>
-                  </Grid.Column>
-                </Grid>
-              </Modal.Content>
-              <Modal.Actions>
-                <Button negative icon='close' labelPosition='right' content='Close' onClick={this.handleClose}/>
-              </Modal.Actions>
-            </Modal>
+          <Card.Content textAlign='right' extra>
+            <MachineActions machine={this.props.machine} notes={this.props.notes} />
           </Card.Content>
         </Card>
     );
